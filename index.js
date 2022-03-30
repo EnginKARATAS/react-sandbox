e7e39a5dd91b7c54d277acb06fb87d35a1f691fa
engin.karatas@dvu.com.tr <43602725+EnginKARATAS@users.noreply.github.com>
2022-03-30 18:48:29 +0300

    imperative way to coding

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function Navbar(params) {
-    return <h1>Hello anam</h1>
-}
-
-const h1 = document.createElement("h1");
-h1.textContent = "hello bozok world";
-h1.className = "header";
-document.getElementById("root").append(h1);
-
-ReactDOM.render(<Navbar />, document.getElementById("root"));
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
