254be662dc9fa7e60c18b692b2cb92489d295039
engin.karatas@dvu.com.tr <43602725+EnginKARATAS@users.noreply.github.com>
2022-03-30 18:46:11 +0300

    declerative coding idea: tons of line of codes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+function Navbar(params) {
+    return <h1>Hello anam</h1>
+}
+
+const h1 = document.createElement("h1");
+h1.textContent = "hello bozok world";
+h1.className = "header";
+document.getElementById("root").append(h1);
+
+ReactDOM.render(<Navbar />, document.getElementById("root"));
