e058c35d9d95552cf42acd3cddf1e804689e9f3c
engin.karatas@dvu.com.tr <43602725+EnginKARATAS@users.noreply.github.com>
2022-05-30 19:19:51 +0300

    assignment done

---
+++ my-app/src/components/Main.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default function Main(){
+    return (
+      <div>
+        <h1>Main This is the Main which is cool page</h1>
+      </div>
+    )
+  }
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default function Navbar(){
+    return (
+      <div>
+        <h1>Navbar This is the Navbar which is cool page</h1>
+      </div>
+    )
+  }
